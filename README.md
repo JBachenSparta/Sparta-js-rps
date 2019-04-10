c55aa8f3bf270625f94d4b336dafe7a0f8928fae
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2019-04-10 07:21:16 +0100

    Most up to daye version of game, user input, cumputer generated move, outcome of each game determined, README file updated

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Sparta-js-rps
+
+## Task
+
+* Create a web application running rock, paper, scissors.  
+  - Prompt player input through the browser.
+  - Create a random move for the computer.
+  - Play the best of five rounds.
+  - Check for a winner after each round.
+  - Display the score after each round.
+  -	Display the overall winner best of out of five games
+
+### Notes
+
+Prompts user for input.
+
+Generates random computer input.
+
+Discovers winner of each round.
+
+Plays five rounds.
+
+Code can definitely be made DRY, especially for the instance of 'if' 'else if' statements in the start function.  
+Further work needs to be to calculate running total, and eventually overall winner.
--- main.js
+++ main.js
@@ -1,40 +1,49 @@
 
-
-var human = prompt("Human input");
-
-var computer = Math.floor((Math.random() * 3 ) + 1);
-  if (computer = 1){
-    computer = "r"
-  }
-  else if (computer = 2) {
-    computer = "p"
-  }
-  else {
-    computer = "s"
-  }
-
-
-function match(human, computer){
-
-  if (human === computer){ return "Drawn" }
-
-  if (human == "r" && computer =="p"){ return "Lose" }
-  else if (human == "r" && computer == "s"){ return "Won" }
-
-  if (human == "p" && computer =="r"){ return "Won" }
-  else if (human == "p" && computer == "s"){ return "Lost" }
-
-  if (human == "s" && computer =="r"){ return "Lost" }
-  else if (human == "s" && computer == "p"){ return "Won" }
-
+//function to prompt user input, returns all strings to lowercase
+function GetHumanInput (){
+  var humanInput = prompt("Please enter either:\r\n(r) rock \r\n(p) paper \r\n(s) scissors").toLowerCase ();
+  return humanInput
 }
 
-function tally (array){
-  tally = [];
+//generate Random number function
+function getRandomComputerInput (){
+  var computerRandom = Math.floor((Math.random() * 3 ) + 1);
+
+  if (computerRandom == 1){
+    computerRandom = "r"
+    return computerRandom
+  }
+  else if (computerRandom == 2){
+    computerRandom = "p"
+    return computerRandom
+  }
+  else if (computerRandom == 3){
+    computerRandom = "s"
+    return computerRandom
+  }
+}
+
+//Function start, calls function getRandomComputerInput and GetHumanInput and then compares the result of each, resulting in either a win draw or lose which is outputted to console
+function start () {
   for (var i = 0; i < 5; i++) {
+    var word = "";
+    
+    var inputHuman = GetHumanInput ();
+    var inputComputer = getRandomComputerInput();
+
+     if (inputHuman === inputComputer) { word += "Drawn" };
+
+     if (inputHuman == "r" && inputComputer == "s") { word += "Win" }
+     else if (inputHuman == "r" && inputComputer == "p") { word += "Lose"  };
+
+     if (inputHuman == "P" && inputComputer == "r") { word += "Win"  }
+     else if (inputHuman == "p" && inputComputer == "s") { word += "Lose" };
+
+     if (inputHuman == "s" && inputComputer == "p") { word += "Win"  }
+     else if (inputHuman == "S" && inputComputer == "r") { word += "Lose"};
+
+     console.log (inputHuman, inputComputer, word)
   }
 }
-
-
-var result = match(human, computer);
-alert (human + " vs " + computer + " you have " + result);
+//initiates the start function
+start ();
